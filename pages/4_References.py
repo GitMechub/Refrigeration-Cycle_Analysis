e9ebd958f9d2fdeddb9538f391fff4a29c37d85d
MecHub <144913824+GitMechub@users.noreply.github.com>
2024-12-12 18:50:27 -0300

    Update 4_References.py

--- pages/4_References.py
+++ pages/4_References.py
@@ -28,47 +28,11 @@ hide_menu = '''
         '''
 st.markdown(hide_menu, unsafe_allow_html=True)
 
-st.header("Refrigeration Cycle Analysis v1.0.0", divider="gray", anchor=False)
+st.header("References", anchor=False, divider='gray')
+st.write("1.   CoolProp: http://www.coolprop.org/coolprop/HighLevelAPI.html#parameter-table")
+st.write("2.   MORAN, M.J.; SHAPIRO, H.N.; BOETTNER, D.D.; BAILEY, M.B. Fundamentals of Engineering Thermodynamics. 8. ed. Hoboken: Wiley, 2014. ISBN 9781118832301.")
+st.write("3.   STEWART, Susan W.; SHELTON, Sam V. The usefulness of entropic average temperatures. In: Proceedings of the ASME International Mechanical Engineering Congress and Exposition. Anaheim, California, 2004. p. 1-6. DOI: 10.1115/IMECE2004-60235.")
+st.write("4.   SEYAM, Shaimaa. Energy and Exergy Analysis of Refrigeration Systems. 2019. ISBN 978-1-83880-666-8. DOI 10.5772/intechopen.88862.")
+st.write("5.   TAO, Y.B.; HE, Y.L.; TAO, W.Q. Exergetic analysis of transcritical CO2 residential air-conditioning system based on experimental data. State Key Laboratory of Multiphase Flow in Power Engineering, School of Energy & Power Engineering, Xi’an Jiaotong University, Xi’an, Shaanxi 710049, China, 2009.")
+st.write("6.   PATIL, Omprakash S.; SHET, Shrikant A.; JADHAO, Manish; AGRAWAL, Neeraj. Energetic and exergetic studies of modified CO2 transcritical refrigeration cycles. Departamento de Engenharia Mecânica, Dr. B. A. Technological University Lonere, Raigad, Maharashtra 402103, Índia, 2020.")
 
-st.markdown('''
-## About Gear Train Sizer:
-
-Spur Gear Sizing is a Python app designed to help you quickly and easily size a compound spur gear train according to Norton's "Machine Design: An Integrated Approach" methodology and AGMA standards.
-
-## How it Works:
-
-To use Gear Train Sizer, you simply need to input a few key parameters:
-- Distance between the first and last shafts
-- Torque and resistance requirements
-- Pressure angle
-- Selected material for the gears
-
-The app will then generate a detailed table showing the main dimensions, stresses, and safety factors for each gear in the train. It will also display the full configuration, including each gear's identification number.
-In addition, Gear Train Sizer will create a separate sheet with the precise coordinates of each gear's profile. You can use these coordinates to easily create a CAD model for each gear.
-
-## Key Features:
-
-- Fast and easy sizing - Input a few parameters and get a complete gear train design
-- Detailed analysis - Table shows dimensions, stresses, safety factors for each gear
-- Full configuration - Displays complete gear train layout with IDs
-- CAD integration - Generates gear profile coordinates for CAD modeling
-
---- 
-
-- Tutorial Video:
-
-  [![YouTube](https://img.shields.io/badge/YouTube-FF0000?style=for-the-badge&logo=youtube&logoColor=white)](https://youtu.be/T1YwprHeOw4)
-
-- How can you help me to improve this code:
-
-  [![YouTube](https://img.shields.io/badge/YouTube-FF0000?style=for-the-badge&logo=youtube&logoColor=white)](https://www.youtube.com/watch?v=z66-aUrW6dE&t=58s)
-
-  ---
-  '''
-            )
-
-path2 = os.path.dirname(__file__)
-my_file2 = path2 + '/images/Thumb_Mechub.png'
-img2 = Image.open(my_file2)
-
-st.image(img2)
